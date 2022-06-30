4a2d564f394ff0e6f8e2f9efe97b049be423eada
darkpc1412 <102814997+darkpc1412@users.noreply.github.com>
2022-06-30 17:04:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,8 +11,51 @@ from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR   
+
+
 from utils import temp
+
+
+from pyrogram import Client
+import os
+
+
+from config import *
+
+class Bot:
+    bot = Client(
+        "shortener",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        bot_token=BOT_TOKEN,
+        plugins=dict(root="plugins")
+
+    )
+
+
+TOKEN = os.environ.get("TOKEN", "")
+
+APP_ID = int(os.environ.get("APP_ID", ""))
+
+API_HASH = os.environ.get("API_HASH", "")
+
+if __name__ == "__main__" :
+    plugins = dict(
+        root="plugins"
+    )
+    app = Client(
+        "renamer",
+        bot_token=TOKEN,
+        api_id=APP_ID,
+        api_hash=API_HASH,
+        plugins=plugins
+    )
+
+
+
+
+
 
 class Bot(Client):
 
@@ -21,12 +64,13 @@ class Bot(Client):
             session_name=SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
+            bot_token=BOT_TOKEN,            
             workers=50,
             plugins={"root": "plugins"},
             sleep_threshold=5,
+            parse_mode="html",
         )
-
+    
     async def start(self):
         b_users, b_chats = await db.get_banned()
         temp.BANNED_USERS = b_users
@@ -35,6 +79,7 @@ class Bot(Client):
         await Media.ensure_indexes()
         me = await self.get_me()
         temp.ME = me.id
+        temp.MENTION = me.mention
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
         self.username = '@' + me.username
